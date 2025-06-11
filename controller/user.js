32396131832d40976306a5b9648d8ab34f59ade7
Shahroz Hassan <s.shahroz.h.a@gmail.com>
2025-06-11 23:18:48 +0500

    code wrap in try catch

--- controller/user.js
+++ controller/user.js
@@ -1,29 +1,33 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/user";
-import {inngest} from "../inngest/client";
+import { inngest } from "../inngest/client";
 
+export const signup = async (req, res) => {
+  const { email, password, skills = [] } = req.body;
 
-export const signup = async(req,res)=>{
-    const {email,password,skills = []} = req.body;
+ try{
+     const hashed = bcrypt.hash(password, 10);
 
-    const hashed = bcrypt.hash(password,10);
-    
-    const user = await User.create({email,password: hashed, skills});
+  const user = await User.create({ email, password: hashed, skills });
 
-    //INNGEST FIRE
-    
-    await inngest.send({
-        name : user/signup,
-        data : {
-            email
-        }
-    });
+  //INNGEST FIRE
 
-    const jwt = jwt.sign({
-        _id : user._id,
-        role: user.role},
+  await inngest.send({
+    name: user / signup,
+    data: {
+      email,
+    },
+  });
+
+  const jwt = jwt.sign(
+    {
+      _id: user._id,
+      role: user.role,
+    },
     process.env.JWT_SECRET
-    );
-
-}
+  );
+ }catch(error){
+    console.error("Error in inngest event", error.message);
+ }
+};
