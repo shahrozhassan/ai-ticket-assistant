f1f4fee736abdbb3e85e1ed9228c5965f1abf7b0
Shahroz Hassan <s.shahroz.h.a@gmail.com>
2025-06-11 23:04:52 +0500

    password bcrypt

--- controller/user.js
+++ controller/user.js
@@ -2,3 +2,14 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/user";
 import {inngest} from "../inngest/client";
+
+
+export const signup = async(req,res)=>{
+    const {email,password,skills = []} = req.body;
+
+    const hashed = bcrypt.hash(password,10);
+    
+    const user = await User.create({email,password: hashed, skills});
+
+    
+}
